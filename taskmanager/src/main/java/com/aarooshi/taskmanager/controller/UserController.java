fac56783fdeb048962f48b72cea6bee1d2aaa431
padhglobal <aarooshismartzone@gmail.com>
2026-05-31 15:58:04 +0530

    Update UserController.java

--- taskmanager/src/main/java/com/aarooshi/taskmanager/controller/UserController.java
+++ taskmanager/src/main/java/com/aarooshi/taskmanager/controller/UserController.java
@@ -3,7 +3,9 @@ package com.aarooshi.taskmanager.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,7 @@ import com.aarooshi.taskmanager.entity.User;
 import com.aarooshi.taskmanager.repository.UserRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/users")
@@ -25,7 +28,34 @@ public class UserController {
 
     @PostMapping // the (same) url /api/users is called under POST method
     public User createUser(@RequestBody User user) {
-        return userRepository.save(user); //Add a user, and save the details.
+        return userRepository.save(user); // Add a user, and save the details.
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User Not found"));
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User Not Found"));
+
+        user.setFName(updatedUser.getFName());
+        user.setLName(updatedUser.getLName());
+        user.setEmail(updatedUser.getEmail());
+        user.setPassword(updatedUser.getPassword());
+        user.setRole(updatedUser.getRole());
+
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+
+        return "User Deleted Successfully";
     }
 }
 
